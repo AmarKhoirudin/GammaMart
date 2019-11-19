8d5e03241125535725fdd98f66a97e6a5c3fde54
albae69 <ahmadalbaihaqi69@gmail.com>
2019-11-19 15:53:26 +0700

    login done

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,15 +1,36 @@
-import React from "react";
-import "./Login.scss";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { fab } from "@fortawesome/free-brands-svg-icons";
+import React from 'react';
+import './Login.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faUserCircle, faLock } from '@fortawesome/free-solid-svg-icons';
 
-// library.add(fab);
+import Logo from '../../assets/img/logo.svg';
+
+library.add(faUserCircle, faLock);
 
 const Login = () => {
   return (
-    <div className="login">
-      <h1>LOGIN</h1>
+    <div className='login'>
+      <div className='login-wrap'>
+        <div className='logo'>
+          <img src={Logo} alt='' />
+          <p>GPOS</p>
+        </div>
+        <div className='form'>
+          <input type='text' placeholder='username' className='username' />
+          <i>
+            <FontAwesomeIcon icon={faUserCircle} className='user' />
+          </i>
+          <input type='password' placeholder='password' className='password' />
+          <i>
+            <FontAwesomeIcon icon={faLock} className='pass' />
+          </i>
+        </div>
+        <div className='login-btn'>
+          <span>LOGIN</span>
+        </div>
+      </div>
     </div>
   );
 };
