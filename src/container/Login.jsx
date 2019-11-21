be90dd60fab61a93fdedd36d68d3e9317502f5b2
albae69 <ahmadalbaihaqi69@gmail.com>
2019-11-21 14:28:16 +0700

    bae lupa ngapain tapi commit aja biar ga bentrok

--- src/container/Login.jsx
+++ src/container/Login.jsx
@@ -1,61 +1,64 @@
-import React, { Component } from "react";
-import LoginCon from "../components/login/Login";
-import Axios from "axios"; 
-import { Redirect } from "react-router-dom"
+import React, { Component } from 'react'
+import LoginCon from '../components/login/Login'
+import Axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
 class Login extends Component {
   state = {
-    name: "",
-    password: "",
+    name: '',
+    password: '',
     redirect: false
-  };
+  }
   onChangeLogin = e => {
     this.setState(
       {
         [e.target.name]: e.target.value
       },
       () => {
-        console.log(this.state);
+        console.log(this.state)
       }
-    );
-  };
+    )
+  }
 
   onSubmitLogin = e => {
-    e.preventDefault();
+    e.preventDefault()
     const dataLogin = {
-      name : this.state.name,
+      name: this.state.name,
       password: this.state.password
     }
-    Axios.post("https://rocky-refuge-01694.herokuapp.com/api/login", dataLogin).then(res => {
+    Axios.post(
+      'https://rocky-refuge-01694.herokuapp.com/api/login',
+      dataLogin
+    ).then(res => {
       console.log(res)
-      localStorage.setItem("token" , res.data.access_token)
+      localStorage.setItem('token', res.data.access_token)
       this.setState({
         redirect: true
       })
     })
-  };
+  }
   render() {
-    const { name, password } = this.state;
-    const { onChangeLogin, onSubmitLogin } = this;
+    const { name, password } = this.state
+    const { onChangeLogin, onSubmitLogin } = this
 
-    if (localStorage.getItem("token")) {
-      return <Redirect to="/chat" />
+    if (localStorage.getItem('token')) {
+      return <Redirect to='/' />
     }
     return (
-      <div>
+      <>
         <LoginCon
           // name
-          nama="name"
+          nama='name'
           valueName={name}
           // password
-          namePassword="password"
+          namePassword='password'
           valuePassword={password}
           //   sama
           onChange={onChangeLogin}
           onSubmit={onSubmitLogin}
         />
-      </div>
-    );
+      </>
+    )
   }
 }
-export default Login;
+export default Login
